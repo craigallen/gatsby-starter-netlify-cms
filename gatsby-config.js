5856368832d077ffa04bec51ff64729e0788efa7
Craig Allen <craig@openwavesdesign.com>
2021-03-04 10:41:42 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Craig Allen - Front-end developer',
+    title: 'Gatsby + Netlify CMS Starter',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
